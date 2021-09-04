8f1ded801631c4ca3673bb5c65fa287658ad04d6
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-09-03 20:41:37 -0500

    before CSS

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,4 +1,40 @@
 package org.launchcode.codingevents.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("events")
 public class EventController {
+
+    private static Map<String, String> events = new HashMap();
+
+    //lives @ / events
+    @GetMapping
+    public String displayAllEvents(Model model){
+        model.addAttribute("events", events);
+    return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    //lives @ /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+     events.put(eventName, eventDescription);
+        return "redirect:/events";
+    }
 }
