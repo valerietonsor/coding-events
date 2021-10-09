fb7f800378ca2cd327339a99dab253af2baeb346
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-10-08 19:06:08 -0500

    initial commit

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,13 +2,13 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,24 +19,60 @@ import java.util.Map;
 public class EventController {
 
 
-
     //lives @ / events
     @GetMapping
-    public String displayAllEvents(Model model){
+    public String displayAllEvents(Model model) {
         model.addAttribute("events", EventData.getAll());
-    return "events/index";
+        return "events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
     //lives @ /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-     EventData.add(new Event(eventName, eventDescription));
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "events/create";
+        }
+        EventData.add(newEvent);
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(Model model, @RequestParam(required = false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        model.addAttribute("event", EventData.getById(eventId));
+        model.addAttribute("title", "Edit Event ${name}(id=${eventId})");
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(@RequestParam int eventId, @RequestParam String name, @RequestParam String description) {
+       Event event = EventData.getById(eventId);
+       event.setName(name);
+       event.setDescription(description);
         return "redirect:/events";
     }
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,21 +1,55 @@
 package org.launchcode.codingevents.models;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
 import java.util.Objects;
+import javax.validation.*;
+import javax.validation.constraints.*;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
 
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank(message = "Name is required.")
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Location is required.")
+    private String location;
+
+    @AssertTrue(message = "Registration must be required.")
+    private boolean registrationRequired;
+
+    @Positive(message = "Must be a number over zero.")
+    private int maxAttendees;
+
+    @Email(message = "Invalid Email. Try again.")
+    @NotBlank(message = "Email is required.")
+    private String contactEmail;
+
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail, String location, boolean registrationRequired, int maxAttendees, EventType type) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.registrationRequired = registrationRequired;
+        this.maxAttendees = maxAttendees;
+        this.type = type;
+
+    }
+
+    public Event(){
         this.id = nextId;
         nextId++;
-    }
+    };
 
     public String getName() {
         return name;
@@ -35,6 +69,46 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public int getMaxAttendees() {
+        return maxAttendees;
+    }
+
+    public void setMaxAttendees(int maxAttendees) {
+        this.maxAttendees = maxAttendees;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     @Override
